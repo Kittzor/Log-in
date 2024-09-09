88fe2c5b0dc5783e8541f9dfb0341dee89522c71
Christoffer Kjellgren <kjellgrenchristoffer@gmail.com>
2024-09-09 15:43:36 +0200

    made a log in/log out function

--- script.js
+++ script.js
@@ -15,30 +15,30 @@ const password = "1234";
 
 //log in function
 logInBtn.addEventListener("click", () => {
-    if(usernameInp.value === username && passwordInp.value === password){
-        //Add and remove class hide to the elements.
-        welcome.classList.add("hide")
-        loggedIn.classList.remove("hide")
-        localStorage.setItem("username", username)
-        usernameInp.value = "";
-        passwordInp.value = "";
-    } else{
-        welcome.classList.add("hide")
-        error.classList.remove("hide")
-    }
-})
+  if (usernameInp.value === username && passwordInp.value === password) {
+    //Add and remove class hide to the elements.
+    welcome.classList.add("hide");
+    loggedIn.classList.remove("hide");
+    localStorage.setItem("username", username);
+    usernameInp.value = "";
+    passwordInp.value = "";
+  } else {
+    welcome.classList.add("hide");
+    error.classList.remove("hide");
+  }
+});
 
 //log out function
-logOutBtn.addEventListener("click", () =>{
-    localStorage.clear();
-    loggedIn.classList.add("hide")
-    welcome.classList.remove("hide")
-})
+logOutBtn.addEventListener("click", () => {
+  localStorage.clear();
+  loggedIn.classList.add("hide");
+  welcome.classList.remove("hide");
+});
 
 //go back to main page
 backBtn.addEventListener("click", () => {
-    error.classList.add("hide")
-    welcome.classList.remove("hide")
-    usernameInp.value = "";
-    passwordInp.value = "";
-})
+  error.classList.add("hide");
+  welcome.classList.remove("hide");
+  usernameInp.value = "";
+  passwordInp.value = "";
+});
